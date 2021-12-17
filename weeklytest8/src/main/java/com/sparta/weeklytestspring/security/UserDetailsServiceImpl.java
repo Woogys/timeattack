bb3eee4be8dc0c520a7dd243d676f4256e692152
Woogys <apolontes@hanmail.net>
2021-12-17 18:52:36 +0900

    timeattack test 21.12.17

---
+++ weeklytest8/src/main/java/com/sparta/weeklytestspring/security/UserDetailsServiceImpl.java
@@ -0,0 +1,19 @@
+package com.sparta.weeklytestspring.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Can't find " + username));
+
+        return new UserDetailsImpl(user);
+    }
+}
